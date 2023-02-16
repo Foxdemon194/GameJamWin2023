8de5af4446bad3aac92818d1e534ac35304b6892
DV <david_ventura_m@hotmail.com>
2023-02-16 05:36:49 -0500

    finished Main Menu. Made Level1 Scene and SettingsScene. Made a scene manager script

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadSettingsMenu()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+}

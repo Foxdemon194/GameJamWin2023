8f262348f8acba0479177c03f1af5999d48892da
Severencir <SeverencirDev@gmail.com>
2023-02-18 22:28:50 -0600

    fixed small thing

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -6,15 +6,11 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
-
-<<<<<<< HEAD
+    
     PauseMenuMan pauseMenuMan;
 
 
     public void LoadLevel1() //this loads the tutorial, not level 1
-=======
-    public void LoadTutorial() //this loads the tutorial, not level 1
->>>>>>> 75e52f8f792a87d8b627afd29b21016685594ef5
     {
         SceneManager.LoadScene("Tutorial");
         Time.timeScale = 1;
